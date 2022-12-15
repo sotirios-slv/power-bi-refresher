e8bfaf776e9e3a859184ea98ef871d62d6912cdb
salpanis <salpanis@gmail.com>
2022-12-15 12:25:51 +1100

    ONe more time

--- pbi-app/api/index.py
+++ pbi-app/api/index.py
@@ -5,8 +5,8 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        # self.send_header('Content-type','text/plain')
-        # self.end_headers()
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
         # self.wfile.write('Hello world!'.encode('utf-8'))
         credential = InteractiveBrowserCredential()
         access_token_class = credential.get_token('https://analysis.windows.net/powerbi/api/.default')
