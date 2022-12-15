936ec5c975abf32473f4b73ea32ea3bef4af9884
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2022-12-16 10:00:21 +1100

    Switched to flask

--- pbi-app/api/index.py
+++ pbi-app/api/index.py
@@ -1,15 +1,7 @@
-from http.server import BaseHTTPRequestHandler
-from azure.identity import InteractiveBrowserCredential
+from flask import Flask
 
-class handler(BaseHTTPRequestHandler):
+app = Flask(__name__)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        # self.wfile.write('Hello world!'.encode('utf-8'))
-        credential = InteractiveBrowserCredential()
-        access_token_class = credential.get_token('https://analysis.windows.net/powerbi/api/.default')
-        access_token=access_token_class.token
-        self.wfile.write(f'{access_token}'.encode('utf-8'))
-        return
+@app.route('/')
+def home():
+    return 'Hello world!'
