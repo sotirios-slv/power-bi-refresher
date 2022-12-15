5c8acd2335477824f5b0d27dd4d1c1f21b414c18
salpanis <salpanis@gmail.com>
2022-12-15 11:35:18 +1100

    Added api endpoint and call in app

---
+++ pbi-app/src/components/Login.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const Login = () => {
+
+  useEffect(() => {
+    const getData = async() => {
+      const response = await axios.get('/api/index')
+      console.log(response)
+    }
+    getData()
+  },[])
+
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
