399118336f5cc03b5dd5e0d8451ee2fc2500452b
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2022-12-16 12:09:02 +1100

    Reverted to test

--- pbi-app/api/index.py
+++ pbi-app/api/index.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler
+from azure.identity import InteractiveBrowserCredential
 
-app = Flask(__name__)
+class handler(BaseHTTPRequestHandler):
 
-@app.route('/')
-def home():
-    return 'Hello world!'
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        # self.wfile.write('Hello world!'.encode('utf-8'))
+        credential = InteractiveBrowserCredential()
+        access_token_class = credential.get_token('https://analysis.windows.net/powerbi/api/.default')
+        access_token=access_token_class.token
+        self.wfile.write(f'{access_token}'.encode('utf-8'))
+        return
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello world!'
